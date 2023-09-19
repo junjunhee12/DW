3cd59348554a1cae31ffa11ee86c18c4abaad603
junjunhee12 <wwg0328@naver.com>
2023-09-19 16:17:58 +0900

    클릭 이벤트

---
+++ 20230919/button_click_envet/script.js
@@ -0,0 +1,30 @@
+// const title = document.getElementById('title')
+// console.log(title)
+// 버튼을 가져와서 변수에 담기
+const button = document.getElementById('btn')
+console.log(button)
+
+// 컨테이너 가져와서 변수에 담기
+const container = document.querySelector('.container')
+console.log(container)
+//버튼 클랙했을 때 컨테이너에 bg 클래스를 추가한다.
+button.addEventListener('click', ()=>{
+    container.classList.add('bg');
+})
+
+
+// 제목을 클릭했을 때, alert 창에 클릭 이라는 문구가 출력
+// addEventListener( '이벤트 이름 ' , 실행할 함수)
+title.addEventListener('click', ()=>{
+    // alert('클릭')
+
+
+    if(title.style.color === 'red'){
+        title.style.color = 'black'
+    }
+    else{
+        title.style.color = 'red'
+    }
+})
+
+
