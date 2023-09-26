d3b5c603027b2435cc190e8a9edfcb808a51e870
junjunhee12 <wwg0328@naver.com>
2023-09-26 12:34:24 +0900

    counter 예제 수정

--- 20230926/counter/script.js
+++ 20230926/counter/script.js
@@ -5,22 +5,37 @@ const app = document.getElementById('app')
 
 // 반복문을 이용해서 dataList안에 있는 data-counter값을 콘솔로 출력
 
-for(let i = 0; i < dataList.length; i++){
-    const target = (dataList[i].dataset.counter);
-    const timerText = dataList[i].querySelector('.timer')
-    const step =Math.floor(target /200)
-    console.log(step);
-    index = 0;
-    setInterval(()=>{
-        
-        // console.log(index);
-        if( index < target){
+// for (let i = 0; i < dataList.length; i++) {
+//     const target = (dataList[i].dataset.counter);
+//     const timerText = dataList[i].querySelector('.timer')
+//     const step = Math.floor(target / 200)
+//     console.log(step);
+//     let index = 0;
+//     setInterval(() => {
 
-            index = index + step;
-            // console.log(index)
-            timerText.textContent = index 
-        }
+//         // console.log(index);
+//         if (index < target) {
 
-        
-    },1)
-}
+//             index = index + step;
+//             // console.log(index)
+//             timerText.textContent = index
+//         }
+
+
+//     }, 1)
+// }
+
+dataList.forEach(item => {
+    let index = 0
+    const target = (item.dataset.counter);
+    const timerText = item.querySelector('.timer')
+    const step = Math.floor(target / 200)
+    setInterval(() => {
+        if (index < target) {
+
+                        index = index + step;
+                        // console.log(index)
+                        timerText.textContent = index
+                    }
+    })
+})
