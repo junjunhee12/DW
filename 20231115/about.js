f84b3a8e9fbfcb8fec6cf52e3c2ac64ab87b3cc8
junjunhee12 <wwg0328@naver.com>
2023-11-17 12:49:54 +0900

    부트스트랩 사용

---
+++ 20231115/about.js
@@ -0,0 +1,37 @@
+(function () {
+  const spanEl = document.querySelector("main h2 span");
+  const txtArr = [
+    " Web Publisher",
+    " Front-End Developer",
+    " Web UX Designer",
+    " UX Designer",
+    " Back-END Developer",
+  ];
+
+  let index = 0;
+  let currentTxt = txtArr[index].split("");
+
+  function writeTxt() {
+    spanEl.textContent += currentTxt.shift();
+    if (currentTxt.length !== 0) {
+      setTimeout(writeTxt, Math.floor(Math.random() * 100));
+    } else {
+      currentTxt = spanEl.textContent.split("");
+      setTimeout(deleteTxt, 3000);
+    }
+  }
+
+  function deleteTxt() {
+    currentTxt.pop();
+    spanEl.textContent = currentTxt.join("");
+    if (currentTxt.length !== 0) {
+      setTimeout(deleteTxt, Math.floor(Math.random() * 100));
+    } else {
+      index = (index + 1) % txtArr.length;
+      currentTxt = txtArr[index].split("");
+      writeTxt();
+    }
+  }
+
+  writeTxt();
+})();
