1cadac60b2fd0a6eb7448f29627b3568aa549775
junjunhee12 <wwg0328@naver.com>
2023-11-22 12:27:11 +0900

    파이어베이스

--- resources/JS/firebase.js
+++ resources/JS/firebase.js
@@ -1,11 +1,17 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
+// import 추가
 import {
   getFirestore,
   collection,
   getDocs,
+  getDoc,
   setDoc,
   addDoc,
   doc,
+  deleteDoc,
+  updateDoc,
+  deleteField,
 } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-firestore.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-analytics.js";
 
@@ -23,17 +29,48 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getFirestore(app);
-// console.log(db);
 
-// 가져오기
 async function getDatas(collectionName) {
   const querySnapshot = await getDocs(collection(db, collectionName));
   return querySnapshot;
 }
 
-// 추가하기
-async function addDatas(collectionName, dataObj) {
-  await setDoc(doc(db, collectionName, "member"), dataObj);
+// doc(, 컬렉션(배열), "문서 ID"), {들어갈 객체}
+async function addDatas(collectionName, dataobj) {
+  // 문서ID 부여
+  //   await setDoc(doc(db, "member", "member1"), dataobj);
+  //   문서ID 자동
+  await addDoc(collection(db, collectionName), dataobj);
 }
 
-export { db, getDocs, collection, getDatas, setDoc, addDoc, doc, addDatas };
+async function deleteDatas(collection, docId) {
+  await deleteDoc(doc(db, collection, docId));
+}
+
+async function updateDatas(collectionName, docId, updateInfoObj) {
+  const docRef = await doc(db, collectionName, docId);
+  const docData = await getDoc(docRef);
+  console.log(docData.data());
+  // debugger;
+  // 문서 필드 데이터 수정
+  // 해당 필드에 있는 것과 맞지 않다면 추가를 한다.
+  // updateDoc(수정할 문서 레퍼런스(ref), {수정할 데이터 객체형식})
+  await updateDoc(docRef, updateInfoObj);
+}
+
+// 내보내기
+export {
+  db,
+  getDocs,
+  collection,
+  setDoc,
+  addDoc,
+  getDatas,
+  doc,
+  addDatas,
+  deleteDoc,
+  deleteDatas,
+  updateDatas,
+  updateDoc,
+  deleteField,
+};
